2382d8e076987d87f984b23bd5268ad8c7233b64
garylirocks <gary.li.rocks@gmail.com>
2017-11-02 12:32:31 +1300

    update setup scripts

--- ubuntu-setup/00-setup.sh
+++ ubuntu-setup/00-setup.sh
@@ -5,10 +5,15 @@ sudo apt-get install gnome-tweak-tool
 
 # utilities
 sudo apt-get install curl terminator tmux vim-gnome
+sudo apt-get install exuberant-ctags
 
 # git
 sudo apt-get install git
 
 # flash
 ## Enable "Ubuntu Partner" in "Software & Updates" first
-sudo apt install adobe-flashplugin
+sudo apt-get install adobe-flashplugin
+
+
+# multi-screen config
+sudo apt-get install compizconfig-settings-manager compiz-plugins
--- ubuntu-setup/20-symlink-setup.sh
+++ ubuntu-setup/20-symlink-setup.sh
@@ -3,7 +3,7 @@
 cd ~
 
 DROP_CONFIG=~/drop/config
-CONFIG_HOME=$DROP_CONFIG/home/
+CONFIG_HOME=$DROP_CONFIG/home
 
 # link ssh configs
 ln -s $CONFIG_HOME/.ssh ./
@@ -25,3 +25,4 @@ ln -s $CONFIG_HOME/config/terminator/config ~/.config/terminator/config
 # autostart
 ln -s $CONFIG_HOME/config/autostart/dropboxd.desktop ~/.config/autostart/dropboxd.desktop
 ln -s $CONFIG_HOME/config/autostart/capslockasctrl.desktop ~/.config/autostart/capslockasctrl.desktop
+ln -s $CONFIG_HOME/config/autostart/trackpoint.desktop ~/.config/autostart/trackpoint.desktop
