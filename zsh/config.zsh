e843f4344df9111f662a3b5e8da24f6d5ec0ac40
garylirocks <gary.li.rocks@gmail.com>
2018-10-17 20:23:47 +1300

    update PATH configs

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -4,3 +4,4 @@ set -o vi
 
 # free Ctrl+C for copying, use Ctrl+K for the interruption signal instead
 stty intr ^K
+
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -9,8 +9,3 @@ export PATH="/opt/Postman/:$PATH"
 # ============================
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
-# ============================
-# local npm package scripts
-# ============================
-export PATH="./node_modules/.bin:$PATH"
-
