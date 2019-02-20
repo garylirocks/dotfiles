946772d05d9098c60cb054087de28261b93d9872
garylirocks <gary.li.rocks@gmail.com>
2019-02-20 19:36:20 +1300

    update ubuntu-setup script

--- ubuntu-setup/00-setup.sh
+++ ubuntu-setup/00-setup.sh
@@ -3,18 +3,22 @@
 # tweak fonts, make 'CapsLock' act as 'Ctrl'
 sudo apt-get install gnome-tweak-tool
 
-# utilities
-sudo apt-get install curl terminator tmux vim-gnome
-sudo apt-get install xclip
-sudo apt-get install exuberant-ctags
-
 # git
 sudo apt-get install git
+
+# utilities
+sudo apt-get install curl
+sudo apt-get install terminator tmux 
+sudo apt-get install vim-gnome exuberant-ctags
+sudo apt-get install xclip
+sudo apt-get install markdown
+sudo apt-get install inkscape shutter graphviz
+sudo apt-get install ascii
 
 # flash
 ## Enable "Ubuntu Partner" in "Software & Updates" first
 sudo apt-get install adobe-flashplugin
 
-
 # multi-screen config
 sudo apt-get install compizconfig-settings-manager compiz-plugins
+
