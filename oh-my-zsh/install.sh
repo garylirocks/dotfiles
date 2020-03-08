87b230dcba614aaf1bca33560e04d0551a21cfb2
garylirocks <gary.li.rocks@gmail.com>
2020-03-09 12:40:42 +1300

    fix oh-my-zsh install script

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,6 +1,6 @@
 
-DIR=$(dirname $0)
+DIR=`readlink -f $(dirname $0)`
 
 echo "Install custom oh-my-zsh theme"
 
-ln -s $DIR/gary.zsh-theme ~/.oh-my-zsh/custom/themes/gary.zsh-theme
+ln -s $DIR/gary.zsh-theme ${HOME%%/}/.oh-my-zsh/custom/themes/gary.zsh-theme
