97e97b2defc770d09e246e6a68fe5e4ae1be82d2
garylirocks <gary.li.rocks@gmail.com>
2020-03-16 12:13:16 +1300

    add a separate oh-my-zsh theme for servers

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -3,4 +3,5 @@ DIR=`readlink -f $(dirname $0)`
 
 echo "Install custom oh-my-zsh theme"
 
-ln -s $DIR/gary.zsh-theme ${HOME%%/}/.oh-my-zsh/custom/themes/gary.zsh-theme
+ln -s --force $DIR/gary.zsh-theme ${HOME%%/}/.oh-my-zsh/custom/themes/gary.zsh-theme
+ln -s --force $DIR/server.zsh-theme ${HOME%%/}/.oh-my-zsh/custom/themes/server.zsh-theme
