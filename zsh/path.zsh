e3f61de42e12a44954b69d16f4bf2c0338f4a56f
garylirocks <gary.li.rocks@gmail.com>
2020-03-22 13:18:39 +1300

    update conda settings

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -9,3 +9,8 @@
 # ============================
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
+# ============================
+# conda
+# ============================
+export PATH="$HOME/miniconda3/bin:$PATH"
+
