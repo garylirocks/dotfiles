4f2bc2c207e0ab9b26b1df407108bfafce22c43a
garylirocks <gary.li.rocks@gmail.com>
2020-05-24 12:57:18 +1200

    add zsh-autosuggestions

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -5,3 +5,6 @@ echo "Install custom oh-my-zsh theme"
 
 ln -s --force $DIR/gary.zsh-theme ${HOME%%/}/.oh-my-zsh/custom/themes/gary.zsh-theme
 ln -s --force $DIR/server.zsh-theme ${HOME%%/}/.oh-my-zsh/custom/themes/server.zsh-theme
+
+echo "Install zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
