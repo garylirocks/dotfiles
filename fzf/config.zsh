855356c888a88f4cb9e1f36585f250a52c104fd2
garylirocks <gary.li.rocks@gmail.com>
2020-07-14 11:02:57 +1200

    Update fzf config

--- fzf/config.zsh
+++ fzf/config.zsh
@@ -2,6 +2,10 @@
 if [ -f ~/.fzf.zsh ]; then
     source ~/.fzf.zsh
 
+    # use rg for default file listing, which will exclude files ignored by .gitignore
+    export FZF_DEFAULT_COMMAND='rg --files'
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
     cc() {
         local quickDirs=~/drop/config/quickDirs.txt
         local dir=`cat $quickDirs | fzf --height 10 --layout reverse | sed "s|~|$HOME|"`
