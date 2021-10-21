38aead652da28976c100b4ca109308153386fbf3
Gary Li <gary@proworkflow.com>
2021-10-22 07:45:11 +1300

    Add docker-compose installation script

--- docker/install.sh
+++ docker/install.sh
@@ -23,3 +23,7 @@ sudo apt-get update
 sudo apt-get install -y docker-ce
 
 sudo usermod -a -G docker $(id --user --name)
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
