cb6f97e3fbf8d9fece568efce1172e4bf418582c
garylirocks <gary.li.rocks@gmail.com>
2022-03-01 11:27:33 +1300

    Update path in zsh

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -12,7 +12,7 @@ export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 # ============================
 # conda
 # ============================
-export PATH="$HOME/miniconda3/bin:$PATH"
+# export PATH="$HOME/miniconda3/bin:$PATH"
 
 # ============================
 # pre-commit, pip etc
