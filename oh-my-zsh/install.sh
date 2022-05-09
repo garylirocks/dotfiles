b718102059b3bcb74ded2aa7458ac170ae485113
garylirocks <gary.li.rocks@gmail.com>
2022-05-09 21:50:33 +1200

    Update oh-my-zsh installation script

--- oh-my-zsh/install.sh
+++ oh-my-zsh/install.sh
@@ -1,4 +1,9 @@
+#!/bin/env bash
 
-# install zsh plugins
+# install oh-my-zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# install zsh plugins to "oh-my-zsh/custom" folder,
+#  which is configured in `.zshrc` as custom plugin/theme folder
 git submodule init
 git submodule update
--- zsh/install.sh
+++ zsh/install.sh
@@ -2,5 +2,3 @@
 
 sudo apt-get install -y zsh
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
