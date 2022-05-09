f727e6c8148cd3cbdf3562389e490ab8be96b752
garylirocks <gary.li.rocks@gmail.com>
2022-05-09 21:51:08 +1200

    Add oh-my-posh

---
+++ oh-my-posh/install.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+# install oh-my-posh
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+# install themes
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.json
+rm ~/.poshthemes/themes.zip
+
