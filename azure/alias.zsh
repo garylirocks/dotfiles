ffdeb6d25fc2021ab1d1945705c002fa09842520
garylirocks <gary.li.rocks@gmail.com>
2023-10-06 18:37:15 +1300

    Add alias for az account listing

--- azure/alias.zsh
+++ azure/alias.zsh
@@ -1,3 +1,7 @@
 
-# highlight default account
-alias az-list-sub=$'az account list -otable --query "[].{Default: isDefault, Name: name, ID: id}" | sed \'/True/s/^/\'`echo -e "\033[32m"`\'/;s/$/\'`echo -e "\e[0m"`/'
+# List all accounts(subscriptions), and highlight the default one
+#
+# The original command is like
+#   az account list -otable --query "[].{Default: isDefault, Name: name, ID: id}" | sed '/True/s/.*/printf "\\e[32m\0\\e[0m"/e'
+# It needs to be quoted properly
+alias az-list-sub=$'az account list -otable --query "[].{Default: isDefault, Name: name, ID: id}" | sed \'/True/s#.*#printf "\\\\e[32m\\0\\\\e[0m"#e\''
