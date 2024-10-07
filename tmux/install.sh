4a2f1ce160e52c50504d904cce0a13336bf3f54f
garylirocks <gary.li.rocks@gmail.com>
2024-10-07 22:56:39 +1300

    Fix tmux script for Mac

--- tmux/install.sh
+++ tmux/install.sh
@@ -1,5 +1,11 @@
 
-sudo apt-get install -y tmux
+if command -v apt-get &> /dev/null; then
+  sudo apt-get install -y tmux
+elif command -v brew &> /dev/null; then
+  brew install tmux
+else
+  echo "!! tmux not installed !!"
+fi
 
 # tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
